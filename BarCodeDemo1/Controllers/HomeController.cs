48b50d2818fd96731ede48e25f095d99da263c9c
Gediminas_1 <gbukauskas@gmail.com>
2023-10-26 20:30:12 +0300

    Bar code demo is ready

---
+++ BarCodeDemo1/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using BarcodeLib;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BarCodeDemo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.barId = "256987_121";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        // BarCode
+        public ActionResult GenerateBarCode(string id)
+        {
+            Barcode barcode = new Barcode();
+            Image img = barcode.Encode(TYPE.CODE39Extended, id, Color.Black, Color.White, 250, 100);
+            
+            var data = ConvertImageToBytes(img);
+            return File(data, "image/jpeg");
+        }
+        private byte[] ConvertImageToBytes(Image img)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+
+        // QRCode
+        public ActionResult GenerateQRCode()
+        {
+            QRCodeGenerator qrCodeGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrCodeGenerator.CreateQrCode("Welcome to DotNETMania", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap bitmap = qrCode.GetGraphic(15);
+            var bitmapBytes = ConvertBitmapToBytes(bitmap);
+            return File(bitmapBytes, "image/jpeg");
+        }
+        private byte[] ConvertBitmapToBytes(Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+
+    }
+}
---
+++ BarCodeDemo1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BarCodeDemo1.MvcApplication" Language="C#" %>
